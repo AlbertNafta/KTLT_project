6db671a1aca2861ad9ab3a1c4aa2f45332219eda
Albert de Nafta <101512551+AlbertNafta@users.noreply.github.com>
2022-03-15 11:22:55 +0700

    Add files via upload
    Bản đầu tiên

---
+++ Project KTLT.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+#include<string.h>
+ 
+using namespace std;
+
+struct staff{
+	string userName;
+	string passWord;
+	string name;
+	int age;
+	string majors;
+	staff *pNext;
+};
+
+struct student{
+	string userName;
+	string passWord;
+	string firstName;
+	string lastName;
+	int gender;
+	string birth;
+	int socialID;
+	int score;
+	staff *pNext;
+};
+
+struct schoolYear{
+	struct courses{
+		string startDate;
+		string endDate;
+		int ID;
+		struct teacher{
+			string name;
+			teacher* pTeach;
+		};
+		
+		
+	};
+};
+
+int main()//this is just a test
+{
+	staff *pFirst=new staff;
+	cin>>pFirst->userName;
+	cout<<pFirst->userName;
+	
+	return 0;
+}
