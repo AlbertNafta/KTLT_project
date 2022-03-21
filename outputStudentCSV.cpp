392bd920d89e738eb32ae1bb0160c62bfde6e311
Albert de Nafta <101512551+AlbertNafta@users.noreply.github.com>
2022-03-21 09:52:18 +0700

    Uploaded 21032022

---
+++ outputStudentCSV.cpp
@@ -0,0 +1,30 @@
+void OutputUser(student *&pHead_s)
+{
+fstream fout;
+  
+    // opens an existing csv file or creates a new file.
+
+  
+    // opens an existing csv file or creates a new file.
+    fout.open("User.CSV", ios::out );
+
+  	
+  	student *pC=pHead_s;
+    // Read the input
+    while(pC!=NULL) 
+	{
+		if(pC!=pHead_s)fout<<"";
+		if(pC->pNext==NULL)fout<<endl;
+        fout <<pC->userName<<","
+			<<pC->passWord<<","
+			<<pC->lastName<<","
+			<<pC->firstName<<","
+			<<pC->gender<<","
+			<<pC->birth<<","
+			<<pC->social_ID<<",";
+	pC=pC->pNext;
+	}
+		
+		
+    
+}
