62253d2b3730d391cc00be0109a8e73100be52d7
Albert de Nafta <101512551+AlbertNafta@users.noreply.github.com>
2022-03-31 13:40:34 +0700

    31032022

--- KTLT project (bản chính).cpp
+++ KTLT project (bản chính).cpp
@@ -24,30 +24,10 @@ struct score{
 	int final;
 	int progress;
 	int average;
-	int ID;
 };
 
-struct aSchoolYear{
-	struct semester{
-		string startDate;
-		string endDate;
-		struct courses{
-			string name;
-			string teacher;
-			string startDate;
-			string endDate;
-			int ID;
-			int credits;
-			int nums;
-			int days;
-			timeTable timetable;
-		};
-	}Fall1,Summer2,Autumm3;
-}year1;
 
-struct timeTable{
-	int week[4][7];
-};
+
 
 void addStudentToClass(classes *&pHead_c,student *&pHead_s);
 bool checkFileWithFstream(string path);
@@ -58,13 +38,18 @@ void inputClasses(classes *&pHead_c);
 void inputTeacherProfile(staff *&pHead_t);
 void inputUserProfile(student *&pHead_s) ;
 void logIn(student *&pHead_s,staff *&pHead_t,string *use,int *role);
-void MenuTeacher(staff *&pHead_t,staff *&pT,student *&pHead_s,string use);
+void MenuTeacher(staff *&pHead_t,student *&pHead_s,string *use,classes *&pHead_c,courses *&head,int &courseAllow);
 void outputClass(classes *&pHead_c);
 void OutputStaff(staff *&pHead_t);
 void OutputUser(student *&pHead_s);
 void removeStudentFromClass(classes *&pHead_c,student *&pHead_s);
 void viewClass(classes *&pHead_c,student *&pHead_s);
-void menuStudent(student *&pHead_s);
+void viewYourclass(classes *&pHead_c,student *&pHead_s,string use);
+void menuStudent(classes *&pHead_c,student *&pHead_s,string *use,int &courseAllow);
+void viewStudentPro(student *&pHead_s,string uses);
+void inputCourse(courses *&head,int &courseAllow);
+void createCourse(courses *&head);
+void viewCourses(courses *&head,student *&pHead_s);
 
 
 int main()//this is just a test
@@ -75,20 +60,27 @@ int main()//this is just a test
 	staff *pT=pHead_t;
 	inputUserProfile(pHead_s);
 	inputTeacherProfile(pHead_t);
+	classes *pHead_c = new classes;
+	courses *head = new courses;
+	inputClasses(pHead_c);
+	int courseAllow=-1;
+	inputCourse(head,courseAllow);
 	int roles=-1;
+	
 	string use;//to know who is login
 	logIn(pHead_s,pHead_t,&use,&roles);
 	if (roles==1)
 	{
-		MenuTeacher(pHead_t,pHead_s,&use);
+		MenuTeacher(pHead_t,pHead_s,&use,pHead_c,head,courseAllow);
 	}	
 	if(roles==0)
 	{
-	 menuStudent(pHead_s);
+	 menuStudent(pHead_c,pHead_s,&use,courseAllow);
 
 	}
 
-//delete []pHead_s;  //chưa rõ có nên dùng không vì nó return ra 3546374343
+//delete []pHead_s;
 //delete []pHead_t;
 	return 0;
 }
+
